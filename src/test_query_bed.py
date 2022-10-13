1c07a678f5887a30996107e55bc534ff73eb6a18
Sara-Elna <elna.elisa@gmail.com>
2022-10-13 10:41:15 +0200

    Less readable, more compact

--- src/test_query_bed.py
+++ src/test_query_bed.py
@@ -7,35 +7,17 @@ from filecmp import cmp
 from filecmp import report
 import os
 
-# My BED file used for tests
-bed = 'data/large.bed'
-
-# List of query files used for tests
-query = ['data/query-1.txt', 'data/query-2.txt', 'data/query-3.txt']
-
-# List of expected outcomes for tests
-expected = ['data/expected-1.txt', 'data/expected-2.txt', 'data/expected-3.txt']
-
-# List of acctual outcome for tests
-output = ['data/test-output-1.bed', 'data/test-output-2.bed', 'data/test-output-3.bed']
-
-# Creating the outcome files for the tests with a while loop
-i = 0
-while i<3:
-    cmd1 = 'echo "" >' + output[i]
-    cmd2 = 'python3.10 query_bed.py ' + bed + ' ' + query[i] + ' -o ' + output[i]
-    os.system(cmd1) # Calling our command to create empty file
-    os.system(cmd2) # Writing output of running query_bed.py on files to output file
-    i+=1
-
 def test_query_bed0():
-    result = report('data/test-output-1.bed', 'data/expected-1.txt') #Comparing test- and expected output.
-    assert result
+    os.system('echo "" > data/test-output-1.bed') # Calling our command to create empty file
+    os.system('python3.10 query_bed.py data/large.bed data/query-1.txt -o data/test-output-1.bed') # Writing output of running query_bed.py on files to output file
+    assert cmp('data/test-output-1.bed', 'data/expected-1.txt') #Comparing test- and expected output.
 
 def test_query_bed1():
-    result = report('data/test-output-2.bed', 'data/expected-2.txt') #Comparing test- and expected output.
-    assert result
+    os.system('echo "" > data/test-output-2.bed') # Calling our command to create empty file
+    os.system('python3.10 query_bed.py data/large.bed data/query-2.txt -o data/test-output-2.bed') # Writing output of running query_bed.py on files to output file
+    assert cmp('data/test-output-2.bed', 'data/expected-2.txt') #Comparing test- and expected output.
 
 def test_query_bed2():
-    result = report('data/test-output-3.bed', 'data/expected-3.txt') #Comparing test- and expected output.
-    assert result
+    os.system('echo "" > data/test-output-3.bed') # Calling our command to create empty file
+    os.system('python3.10 query_bed.py data/large.bed data/query-3.txt -o data/test-output-3.bed') # Writing output of running query_bed.py on files to output file
+    assert cmp('data/test-output-3.bed', 'data/expected-3.txt') #Comparing test- and expected output.
